8d5940fa5fa9f6add438f0c7b520bfc47cd2875a
Ilsun Choi <ilsunchoi@cloudsecuritylab.co.kr>
2025-02-15 19:15:13 +0900

    Update git_commit.sh

--- git_commit.sh
+++ git_commit.sh
@@ -3,4 +3,6 @@ git add .
 git commit -m "첫 번째 커밋"
 #git remote add origin https://github.com/사용자명/레포지토리명.git
 git remote add origin https://github.com/gasbugs/my-cicd-app.git
+git branch main
+git checkout main
 git push -u origin main
